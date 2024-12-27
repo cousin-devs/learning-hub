2d2572d2b4a53ed9423616b634a82d240207d060
imranh02 <imranh@utexas.edu>
2024-12-27 17:40:53 -0600

    fixed contact button

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{ts,tsx}',         
+    './**/*.{ts,tsx}',        
+    './components/ui/**/*.{ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
